1ac2441a47f1298b314565ac9775437fa767fd5b
Joe Wright <joe.wright@noventech.com>
2012-01-23 22:25:00 -0600

    cleaned up code. added comments.

--- yammer.rb
+++ yammer.rb
@@ -9,67 +9,42 @@ require 'pry'
 options = {}
 
 optparse = OptionParser.new do|opts|
- # Set a banner, displayed at the top
- # of the help screen.
- opts.banner = "Usage: yammer.rb [options] ..."
+  opts.banner = "Usage: yammer.rb [options] ..."
 
- # Define the options, and what they do
- options[:update] = false
- opts.on( '-u', '--update', 'Send an update to yammer' ) do
-   options[:update] = true
- end
+  options[:update] = false
+  opts.on( '-u', '--update', 'Send an update to yammer' ) do
+    options[:update] = true
+  end
  
- options[:list] = false
- opts.on( '-l', '--list', 'Get the last 20 updates' ) do
-   options[:list] = true
- end
+  options[:list] = false
+  opts.on( '-l', '--list', 'Get the last 20 updates' ) do
+    options[:list] = true
+  end
 
-# options[:logfile] = nil
-# opts.on( '-l', '--logfile FILE', 'Write log to FILE' ) do|file|
-#   options[:logfile] = file
-# end
-
- # This displays the help screen, all programs are
- # assumed to have this option.
- opts.on( '-h', '--help', 'Display this screen' ) do
-   puts opts
-   exit
- end
+  opts.on( '-h', '--help', 'Display this screen' ) do
+    puts opts
+    exit
+  end
 end
+
+#parse command line arguments and remove parsed flags from ARGV
+optparse.parse!
  
- # Parse the command-line. Remember there are two forms
- # of the parse method. The 'parse' method simply parses
- # ARGV, while the 'parse!' method parses ARGV and removes
- # any options found there, as well as any parameters for
- # the options. What's left is the list of files to resize.
- optparse.parse!
- 
-#some simple functions
+#search users for user with specific id and return that user
 def get_user(users, id)
-  # user = users.select { |f| f['id'] == id }
-  # user.first
-  
-  users.each do |u|
-    if u[:id] == id
-      return u
-    end
-  end
-  
+  user = users.select { |f| f[:id] == id }
+  user.first
 end
 
-
+#configure the yammer authentication parameters
 Yammer.configure do |config|
-    config.consumer_key = 'mWn3PY7sc2znsuZxEMvNUQ'
-    config.consumer_secret = 'AGP2akBsMwybb1AoGBp7RdLc4vfb2l3NY4P6VM'
-    config.oauth_token = 'ZgBliu20ToeIho4Ny4nUcA'
-    config.oauth_token_secret = 'TQ6ppWF3CjeY6ccCFPUvz1N5BLbGjEoXf1jvocFm4k'
-  end
-  #Yammer.update("This is an update from the Yammer gem")
-  # If you have the thread id you want to retrieve 
-  #thread = Yammer.thread(thread_id)
-  # retrieve the most recent 20 messages
-  # messages = Yammer.messages
+  config.consumer_key = ''
+  config.consumer_secret = ''
+  config.oauth_token = ''
+  config.oauth_token_secret = ''
+end
 
+#create new yammer object
 yammer = Yammer.new
 
 if options[:update]
@@ -81,22 +56,16 @@ end
 if options[:list]
   messages = yammer.messages
   
-  users = messages[:references].select {|f| f['type'] == 'user' }
-  # messages[:references].each do |reference|
-  #   puts reference[:type]
-  # end
+  #parse out user objects from references
+  users = messages[:references].select {|f| f[:type] == 'user' }
   
+  #parse each message and look up user names from users array
   messages[:messages].each do |message|
     body = message[:body][:plain]
     created_at = DateTime.parse(message[:created_at]).to_time.getlocal.strftime("%I:%M%p")
     user = get_user(users, message[:sender_id])[:full_name]
     puts user.foreground(:red) + " at " + created_at.foreground(:blue) + " " + body
-    #puts "#{user} at #{created_at}  #{body}"
   end
   
-  #binding.pry
 end
 
-
-
-#binding.pry  
